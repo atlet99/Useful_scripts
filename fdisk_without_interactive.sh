85e50b7d6a32f897b9be317abd027d9f8e16cb39
Zhakhongir R <joha.shadibekov@gmail.com>
2023-01-04 18:10:01 +0600

    Fix GPT partition table issue

--- fdisk_without_interactive.sh
+++ fdisk_without_interactive.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for disk in sda sdb sdc sdd sde sdf sdg sdh; do (echo n; echo p; echo 1; echo; echo; echo t; echo 8e; echo w) | fdisk /dev/$disk 2&>/dev/null; done
+for disk in sda sdb sdc sdd sde sdf sdg sdh; do (echo g; echo n; echo p; echo 1; echo; echo; echo t; echo 8e; echo w) | fdisk /dev/$disk 2&>/dev/null; done
