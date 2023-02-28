5243912107642cb03d81bfa4bff6b0e50cacbecb
Zhakhongir R <joha.shadibekov@gmail.com>
2023-02-28 12:28:19 +0600

    Added Empty and Unused Partition Size Parameters

--- disk-manage/new_get_empty_disk.sh
+++ disk-manage/new_get_empty_disk.sh
@@ -4,7 +4,6 @@
 
 # This script will give you the names of the unformatted and unused partitions that you can use for your purposes. Note that you should be careful when working with disk partitions, as you can easily destroy data and cause other issues if you make a mistake.
 
-
 # Get a list of all disk partitions
 partitions=$(lsblk | awk '$6 == "disk" {print $1}')
 
@@ -16,8 +15,10 @@ for partition in $partitions; do
   # Check if the partition is mounted
   mount=$(lsblk -o MOUNTPOINT /dev/$partition | awk 'NR > 1 {print $1}')
 
-  # If the partition is unformatted and unused, output its name
+  # If the partition is unformatted and unused, output its name and size
   if [ -z "$format" ] && [ -z "$mount" ]; then
-    echo "/dev/$partition"
+    size=$(lsblk -o SIZE /dev/$partition | awk 'NR > 1 {print $1}')
+    echo "/dev/$partition: $size"
   fi
 done
+
